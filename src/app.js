871ed67d87b058ebeba6b3d7f0b1bcde91823ddf
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2023-12-14 09:19:51 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require("express")
 const morgan = require("morgan")
 const cookieParser = require("cookie-parser")
 const sessions = require("express-session")
+var cors = require('cors')
 const { apiV1 } = require("./routes")
 const { connectDb } = require("./db")
 const { UserModel } = require("./models/user")
@@ -16,6 +17,7 @@ app.use(morgan("dev"))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors());
 
 app.use(
   sessions({
